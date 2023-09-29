c01182bedbd15ffed5d43e97d3366cf5f35dd259
AlexSkvorz <alexskvorz@gmail.com>
2023-09-29 16:22:41 +0300

    add new method to calculate user profits

--- incomes_calculations/calc_module.py
+++ incomes_calculations/calc_module.py
@@ -1,16 +1,14 @@
-def calculate_week_user_profits(week_profit, user_deposits, user_overall_profits):
+def calculate_week_user_profits(actual_total_sum, user_deposits, user_overall_profits):
     user_week_profits = {}
+    previous_total_sum = (sum(value for value in user_deposits.values()) +
+                          sum(value for value in user_overall_profits.values()))
+    percent = actual_total_sum / previous_total_sum
 
-    total_sum = sum(value for value in user_deposits.values())
-
-    for user in user_deposits.keys():
-        percent_part_of_deposit = user_deposits[user] * 100 / total_sum
-        user_part_of_week_profit = week_profit * percent_part_of_deposit / 100
-        user_week_profits[user] = user_part_of_week_profit
-
-        if user in user_overall_profits.keys():
-            user_overall_profits[user] += user_part_of_week_profit
-        else:
-            user_overall_profits[user] = user_part_of_week_profit
+    for user, deposit in user_deposits.items():
+        overall_profit = user_overall_profits.get(user, 0)
+        person_deposit_with_profit = deposit + overall_profit
+        user_week_profit = (person_deposit_with_profit * percent) - person_deposit_with_profit
+        user_overall_profits[user] = overall_profit + user_week_profit
+        user_week_profits[user] = user_week_profit
 
     return user_overall_profits, user_week_profits
