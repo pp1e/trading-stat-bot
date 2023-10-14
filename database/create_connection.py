6831c79fde03719ea0b4c7f7473ceed3ba2d119f
Denis Malyutin <malyutin.02@bk.ru>
2023-10-14 11:05:54 +0300

    Fix not create database tables automatically

--- database/create_connection.py
+++ database/create_connection.py
@@ -1,7 +1,14 @@
 import sqlite3
 
 from config.storage_config import STORAGE_CONFIG
+from database import deposit_info_table
+from database import users_rights_table
+from database import weeks_stats_table
 
 
 def create_db_connection():
-    return sqlite3.connect(STORAGE_CONFIG['name'], check_same_thread=False)
+    db_connection = sqlite3.connect(STORAGE_CONFIG['name'], check_same_thread=False)
+    deposit_info_table.create_table(db_connection)
+    users_rights_table.create_table(db_connection)
+    weeks_stats_table.create_table(db_connection)
+    return db_connection
