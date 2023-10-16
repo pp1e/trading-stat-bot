7e10276e6b031126a6b13d37bc5617c7436c3245
AlexSkvorz <alexskvorz@gmail.com>
2023-10-16 14:07:35 +0300

    fix pr remarks

--- telegram_bot/handlers/handle_interact_with_deposit.py
+++
@@ -1,5 +0,0 @@
-from telegram_bot.message_generators.user_rights_message_generator import send_user_rights
-
-
-def handle_interact_with_deposit(chat_id, bot):
-    send_user_rights(bot=bot, chat_id=chat_id)
